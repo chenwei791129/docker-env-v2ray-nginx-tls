357b0d5b6c6dabe72311f19f71d3ff6cf4f8e27e
AwEi <chenwei791129@livemail.tw>
2019-08-14 17:21:35 +0800

    fix bug

--- setup.sh
+++ setup.sh
@@ -20,15 +20,6 @@ if [ -n "${VMESS_ALTERID}" ]; then
   echo $(cat /etc/v2ray/config.json | jq '.inbounds[0].settings.clients[0].alterId = '${VMESS_ALTERID}'') > /etc/v2ray/config.json
 fi
 
-if [ ! -f /etc/nginx/cert/cert.pem ] && [ ! -f /etc/nginx/cert/key.pem ] && [ ! -f /etc/nginx/cert/dhparam.pem ]; then
-  echo '[Info] Start set Weak Diffie-Hellman and the Logjam Attack:'
-  openssl dhparam -out /etc/nginx/cert/dhparam.pem 4096
-  echo '[Info] Start get cert:'
-  /root/.acme.sh/acme.sh --issue -d "${VMESS_HTTP2_DOMAIN}" -w /www --keylength ec-384
-  echo '[Info] Start install cert:'
-  /root/.acme.sh/acme.sh --install-cert -d "${VMESS_HTTP2_DOMAIN}" --key-file /etc/nginx/cert/key.pem --fullchain-file /etc/nginx/cert/cert.pem --capath /etc/nginx/auth-acme/ca.pem --reloadcmd "nginx -s reload" --ecc
-fi
-
 echo $(cat /etc/v2ray/config.json | jq '.inbounds[0] += {"streamSettings":{"network":"ws","wsSettings":{"path":"/v2ray"}}}') > /etc/v2ray/config.json
 
 if [ ${DENY_LAN_ACCESS} == true ]; then
@@ -39,8 +30,18 @@ if [ ${DENY_LAN_ACCESS} == true ]; then
   echo $(cat /etc/v2ray/config.json | jq '. += {"routing":{"rules":[{"type":"field","ip":["geoip:private"],"outboundTag":"blocked"}]}}') > /etc/v2ray/config.json
 fi
 
+if [ ! -f /etc/nginx/cert/cert.pem ] && [ ! -f /etc/nginx/cert/key.pem ] && [ ! -f /etc/nginx/cert/dhparam.pem ]; then
+  echo '[Info] Start set Weak Diffie-Hellman and the Logjam Attack:'
+  openssl dhparam -out /etc/nginx/cert/dhparam.pem 4096
+  echo '[Info] Start get cert:'
+  /root/.acme.sh/acme.sh --issue -d "${VMESS_HTTP2_DOMAIN}" -w /www --keylength ec-384
+  echo '[Info] Start install cert:'
+  /root/.acme.sh/acme.sh --install-cert -d "${VMESS_HTTP2_DOMAIN}" --key-file /etc/nginx/cert/key.pem --fullchain-file /etc/nginx/cert/cert.pem --capath /etc/nginx/auth-acme/ca.pem --reloadcmd "nginx -s reload" --ecc
+fi
+
 echo '[Debug] Dump config.json:'
 echo $(cat /etc/v2ray/config.json)
-
+echo "[Success] Your vmess domain is: ${VMESS_HTTP2_DOMAIN}"
 echo "[Success] Your vmess ID is: $(cat /etc/v2ray/config.json | jq -r '.inbounds[0].settings.clients[0].id')"
 echo "[Success] Your vmess Alter ID is: $(cat /etc/v2ray/config.json | jq -r '.inbounds[0].settings.clients[0].alterId')"
+echo "[Success] Your vmess path is: /v2ray"
